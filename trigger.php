007a7d8a15cb6e4cda17caed02723561ff2556a0
Carsten Bach <mail@carsten-bach.de>
2025-07-31 11:05:07 +0200

    Removed auto-gen files

--- trigger.php
+++ trigger.php
@@ -9,4 +9,8 @@
 // - Wiki files generated in 'wiki' directory but no git repository found
 
 
-// 2. run
+// 2. run: https://github.com/carstingaxion/gatherpress_extract-wp-hooks_workflow-test/actions/runs/16644701562/job/47102476957
+// 
+// - fatal: destination path 'docs/developer/hooks' already exists and is not an empty directory.
+// - Wiki repository not found. Creating wiki directory for output.
+// - Wiki files generated in 'wiki' directory but no git repository found
