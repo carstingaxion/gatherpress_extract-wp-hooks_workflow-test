b821be08342036f868a1afd9410a518f4484c358
Carsten Bach <mail@carsten-bach.de>
2025-07-31 11:03:15 +0200

    Trigger the workflow

--- trigger.php
+++ trigger.php
@@ -2,4 +2,11 @@
 
 // Changes to any .php file will trigger the workflow.
 
-// 1
+// 1. run: https://github.com/carstingaxion/gatherpress_extract-wp-hooks_workflow-test/actions/runs/16644597529/job/47102120220
+// 
+// - fatal: destination path 'docs/developer/hooks' already exists and is not an empty directory.
+// - Wiki repository not found. Creating wiki directory for output.
+// - Wiki files generated in 'wiki' directory but no git repository found
+
+
+// 2. run
